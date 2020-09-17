f9fcd2a82ca173cdc7e0f3a3a3672dfb184ef8f3
Gabriele Sbaiz <gabriele.sbaiz@2601.it>
2020-09-17 16:20:16 +0200

    Add app_name

--- config/unsplash.php
+++ config/unsplash.php
@@ -4,4 +4,5 @@ return [
     'access_key' => env('UNSPLASH_ACCESS_KEY'),
     'store_in_database' => env('UNSPLASH_STORE_IN_DATABASE', false),
     'disk' => env('UNSPLASH_STORAGE_DISK', 'local'),
+    'app_name' => env('UNSPLASH_APP_NAME', 'your_app_name'),
 ];
